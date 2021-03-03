4eb9451534bbcb9e7c17b3aa183029416cfdc182
thavaseelan <rthavaseelan66@gmail.com>
2021-03-03 10:52:33 +0530

    include async, await in chai

--- test/chai-testing.js
+++ test/chai-testing.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const app = require('../index');
+const expect = require('chai').expect;
 
 describe('app', function () {
     it('app shoud return hellow', function () {
@@ -8,5 +9,13 @@ describe('app', function () {
     it('app shoud return perams String', function () {
         const result = app.testString('hi')
         assert.typeOf(result, 'string');
-    })
-})
+    });
+});
+
+describe('#add()', () => {
+    it('2 + 2 is 4', async () => {
+        const p = await app.add(2, 2)
+        console.log(typeof p)
+        expect(p).to.equal(4);
+    });
+});
